20e8bbfa6275dd0ad97eddd74296aae29db6e19b
hoaxdream <hoaxdream@gmail.com>
2021-03-30 02:19:39 +0800

    Font2 patch

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "monospace:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static char *font2[] = { "JoyPixels:pixelsize=15:antialias=true:autohint=true" };
 static int borderpx = 15;
 
 /*
