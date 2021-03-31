7b55d487454f0092207585814e8ca17bef480014
hoaxdream <hoaxdream@gmail.com>
2021-04-01 00:43:54 +0800

    Minor edit

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=15:antialias=true:autohint=true" };
+static char *font = "monospace:size=12:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:size=12:antialias=true:autohint=true" };
 static int borderpx = 15;
 
 /*
