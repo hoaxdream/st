79b1e9cb170ce7964e5f61ed259ed265b1c4a7df
hoaxdream <hoaxdream@gmail.com>
2021-03-31 22:55:30 +0800

    Minor edit

--- config.def.h
+++ config.def.h
@@ -97,30 +97,30 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
   /* 8 normal colors */
-  "#3b4252", /* black   */
-  "#bf616a", /* red     */
-  "#a3be8c", /* green   */
-  "#ebcb8b", /* yellow  */
-  "#81a1c1", /* blue    */
-  "#b48ead", /* magenta */
-  "#88c0d0", /* cyan    */
-  "#e5e9f0", /* white   */
+  "#1d2021", /* black   */
+  "#cc241d", /* red     */
+  "#98971a", /* green   */
+  "#d79921", /* yellow  */
+  "#458588", /* blue    */
+  "#b16286", /* magenta */
+  "#689d6a", /* cyan    */
+  "#a89984", /* white   */
 
   /* 8 bright colors */
-  "#4c566a", /* black   */
-  "#bf616a", /* red     */
-  "#a3be8c", /* green   */
-  "#ebcb8b", /* yellow  */
-  "#81a1c1", /* blue    */
-  "#b48ead", /* magenta */
-  "#8fbcbb", /* cyan    */
-  "#eceff4", /* white   */
+  "#928374", /* black   */
+  "#fb4934", /* red     */
+  "#b8bb26", /* green   */
+  "#fabd2f", /* yellow  */
+  "#83a598", /* blue    */
+  "#d3869b", /* magenta */
+  "#8ec07c", /* cyan    */
+  "#ebdbb2", /* white   */
 
   [255] = 0,
 
   /* more colors can be added after 255 to use with DefaultXX */
-  "#22262e", /* background */
-  "#d8dee9", /* foreground */
+  "#1d2021", /* background */
+  "#ebdbb2", /* foreground */
 };
 
 
@@ -128,10 +128,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-static unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 256;
+unsigned int defaultfg = 259;
+unsigned int defaultbg = 258;
+static unsigned int defaultcs = 256;
+static unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
