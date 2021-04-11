c1f8bedb23c78635864d7a78951ad000c57ba620
hoaxdream <hoaxdream@gmail.com>
2021-04-12 05:24:41 +0800

    Update for void

--- config.mk
+++ config.mk
@@ -7,8 +7,13 @@ VERSION = 0.8.4
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+# Arch
+#X11INC = /usr/X11R6/include
+#X11LIB = /usr/X11R6/lib
+
+# Void
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/X11
 
 PKG_CONFIG = pkg-config
 
